fde96c70993c5fff49ec23251b5511ebe5090504
Andrey Vlasov <59191751+Andrei-Vlasov@users.noreply.github.com>
2020-03-04 12:10:29 +0200

    Добавил функции

--- source.py
+++ source.py
@@ -1,23 +1,29 @@
-f=open("pr_league1.csv", "r")
-matches = [line.strip().split(',') for line in f]
-teamCount = int(matches.pop(0)[0])
-scores = []
-for i in range(teamCount):
-    for j in range(len(matches[i])):
-        if ":" in matches[i][j]:
-            matches[i][j] = matches[i][j].split(':')
-            if matches[i][j][0] < matches[i][j][1]:
-                matches[i][j] = 0
-            elif matches[i][j][0] > matches[i][j][1]:
-                matches[i][j] = 3
-            elif matches[i][j][0] == matches[i][j][1]:
-                matches[i][j] = 1
-for i in range(teamCount):
-    scores.append([matches[i].pop(0), sum(matches[i])])
-for i in range(teamCount):
-    scores[i][0], scores[i][1] = scores[i][1], scores[i][0]
-scores.sort(reverse=True)
-print(scores)
-with open("scoreboard.csv", "w") as result:
-    for i in range(len(scores)):
-        result.write(f"{scores[i][1]},{scores[i][0]}\n")
+def sort_by_score(file):
+    f=open(file, "r")
+    matches = [line.strip().split(',') for line in f]
+    teamCount = int(matches.pop(0)[0])
+    scores = []
+    for i in range(teamCount):
+        for j in range(len(matches[i])):
+            if ":" in matches[i][j]:
+                matches[i][j] = matches[i][j].split(':')
+                if matches[i][j][0] < matches[i][j][1]:
+                    matches[i][j] = 0
+                elif matches[i][j][0] > matches[i][j][1]:
+                    matches[i][j] = 3
+                elif matches[i][j][0] == matches[i][j][1]:
+                    matches[i][j] = 1
+    for i in range(teamCount):
+        scores.append([matches[i].pop(0), sum(matches[i])])
+    for i in range(teamCount):
+        scores[i][0], scores[i][1] = scores[i][1], scores[i][0]
+    scores.sort(reverse=True)
+    return scores
+
+def output_scoreboard(scores):
+    with open("results.csv", "w") as result:
+        for i in range(len(scores)):
+            result.write(f"{scores[i][1]},{scores[i][0]}\n")
+
+scores = sort_by_score("pr_league1.csv")
+output_scoreboard(scores)
